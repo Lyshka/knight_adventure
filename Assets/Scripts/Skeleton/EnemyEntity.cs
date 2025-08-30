af95060c17b62a0bac16bc4acd01314edc28bc7b
Lyshka <lesha_novitskiy@mail.ru>
2025-08-30 16:21:16 +0300

    добавил атаку скелету

--- Assets/Scripts/Skeleton/EnemyEntity.cs
+++ Assets/Scripts/Skeleton/EnemyEntity.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 
+[RequireComponent(typeof(PolygonCollider2D))]
 public class EnemyEntity : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
     private int _currrentHealth;
 
-    public void Start()
+    private PolygonCollider2D _polygonCollider2D;
+
+    private void Awake()
+    {
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Start()
     {
         _currrentHealth = _maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Attack");
     }
 
     public void TakeImage(int damage)
@@ -17,7 +30,17 @@ public class EnemyEntity : MonoBehaviour
         DetectDeath();
     }
 
-    public void DetectDeath()
+    public void PolygonColliderTurnOff()
+    {
+        _polygonCollider2D.enabled = false;
+    }
+
+    public void PolygonColliderTurnOn()
+    {
+        _polygonCollider2D.enabled = true;
+    }
+
+    private void DetectDeath()
     {
         if (_currrentHealth <= 0)
         {
